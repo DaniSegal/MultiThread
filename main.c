919d39713b4e3a72993b07a9bf52770984414bc1
DaniSegal <daniel.seg12@gmail.com>
2024-10-08 17:04:32 +0300

    Implementing a basic thread in main.c

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-int main() {
+
+void *function1() {
+    printf("Thread 1 is working\n");
+    return NULL;
+}
+
+int main(int argc, const char * argv[]) {
+    pthread_t thread1;
+    pthread_create(&thread1, NULL, &function1, NULL);
+    pthread_join(thread1, NULL);
     printf("Hello, World!\n");
     return 0;
 }
