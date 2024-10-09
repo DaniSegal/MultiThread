0d089dee94dcb445315286f4437e691b4942818b
DaniSegal <daniel.seg12@gmail.com>
2024-10-09 17:43:57 +0300

    Working new mailBoxes logic

--- main.c
+++ main.c
@@ -2,15 +2,28 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define NUM_THREADS 4
+#define EXIT_FAILURE 1
+// Global variable to hold the wanted number of threads
+#define NUM_THREADS 7
 
-int mails = 0;
 pthread_mutex_t mutex;
 
-void *function1() {
+// Structure to hold thread arguments
+typedef struct {
+    int *mailBoxes;
+    int mailBoxesNum;
+    int threadNum;
+} thread_args_t;
+
+void *function1(void *args) {
+    thread_args_t *threadArgs = (thread_args_t *)args;
+    int *mailBoxes = threadArgs->mailBoxes;
+    int mailBoxesNum = threadArgs->mailBoxesNum;
+    int threadNum = threadArgs->threadNum;
+
     for (int i = 0; i < 1000000; i++) {
         pthread_mutex_lock(&mutex);
-        mails++;
+        mailBoxes[threadNum % mailBoxesNum]++;
         pthread_mutex_unlock(&mutex);
     }
     return NULL;
@@ -19,17 +32,44 @@ void *function1() {
 int main(int argc, const char * argv[]) {
     int i;
     pthread_t threads[NUM_THREADS];
-    pthread_mutex_init(&mutex, NULL);
+    thread_args_t threadArgs[NUM_THREADS];
+
+    // Initialize the mutex
+    if ((pthread_mutex_init(&mutex, NULL)) != 0) {
+        perror("Failed to initialize mutex");
+        return EXIT_FAILURE;
+    }
 
 
+    srand(time(NULL));
+    int mailBoxesNum = (rand() % NUM_THREADS) + 1;
+    int *mailBoxes = malloc(mailBoxesNum * sizeof(int));
+    if (!mailBoxes) {
+        perror("Failed to allocate memory for mailBoxes");
+        return EXIT_FAILURE;
+    }
+    // Initialize mailBoxes to 0
+    for (i = 0; i < mailBoxesNum; i++) {
+        mailBoxes[i] = 0;
+    }
+    //  initialize thread arguments
+    
     for (i = 0; i < NUM_THREADS; i++) {
-        if (pthread_create(&threads[i], NULL, function1, NULL) != 0) {
+        threadArgs[i].mailBoxes = mailBoxes;
+        threadArgs[i].mailBoxesNum = mailBoxesNum;
+        threadArgs[i].threadNum = i;
+    }
+
+    // Create threads
+    for (i = 0; i < NUM_THREADS; i++) {
+        if (pthread_create(&threads[i], NULL, function1, (void *)&threadArgs[i]) != 0) {
             fprintf(stderr, "Failed to create thread number %d\n", i);
             perror(NULL);
             return i;
         }
         printf("Thread number %d has been created\n", i);
     }
+    // Join threads
     for (i = 0; i < NUM_THREADS; i++) {
         if (pthread_join(threads[i], NULL) != 0) {
             fprintf(stderr, "Failed to join thread number %d\n", i);
@@ -40,7 +80,13 @@ int main(int argc, const char * argv[]) {
     }
 
     pthread_mutex_destroy(&mutex);
-    printf("Mails: %d\n", mails);
+
+    // Print the mailBoxes contents
+    for (i = 0; i < mailBoxesNum; i++) {
+        printf("Mailbox %d: %d\n", i, mailBoxes[i]);
+    }
+
+    free(mailBoxes);
 
     return 0;
 }
